38b5b38bb9e53cf56401b966e58ab0af6ea2abfb
sangili.pandian <chainchelliah@gmail.com>
2018-09-20 18:23:13 +0530

    Jenkins test

--- app/src/main/java/com/example/sangilipandian/sampleci/MainActivity.kt
+++ app/src/main/java/com/example/sangilipandian/sampleci/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.sangilipandian.sampleci
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Jenkins", "Test")
     }
 }
