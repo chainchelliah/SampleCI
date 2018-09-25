361071ca1595c9218e426ef8d9a4d9474cc4ee08
sangili.pandian <chainchelliah@gmail.com>
2018-09-25 16:35:26 +0530

    Androidx migration

--- app/src/main/java/com/example/sangilipandian/sampleci/MainActivity.kt
+++ app/src/main/java/com/example/sangilipandian/sampleci/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.sangilipandian.sampleci
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
